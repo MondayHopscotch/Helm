fceba5d0834dcdc5c85d09fbf2880aed98150e58
Logan Moore <monday.hopscotch@gmail.com>
2016-12-08 22:16:06 -0700

    intial commit of libgdx project

---
+++ desktop/src/com/bitdecay/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bitdecay.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bitdecay.game.Helm;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Helm(), config);
+	}
+}
---
+++ ios/src/com/bitdecay/game/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.bitdecay.game;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.bitdecay.game.Helm;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new Helm(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
