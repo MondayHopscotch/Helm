1a04f81dd63fff3359d3652a96644d1b61591303
Logan Moore <monday.hopscotch@gmail.com>
2016-12-21 22:17:30 -0700

    placeholder title screen with version number

--- core/src/com/bitdecay/game/Helm.java
+++ core/src/com/bitdecay/game/Helm.java
@@ -2,10 +2,11 @@ package com.bitdecay.game;
 
 import com.badlogic.gdx.Game;
 import com.bitdecay.game.screen.GameScreen;
+import com.bitdecay.game.screen.TitleScreen;
 
 public class Helm extends Game {
     @Override
     public void create() {
-        setScreen(new GameScreen(this));
+        setScreen(new TitleScreen(this));
     }
 }
