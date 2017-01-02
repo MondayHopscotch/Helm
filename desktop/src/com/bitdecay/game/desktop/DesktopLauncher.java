19efe6854b153db1f46e478f22f59ec86681c617
Logan Moore <monday.hopscotch@gmail.com>
2017-01-01 23:09:01 -0700

    intial editor code

--- desktop/src/com/bitdecay/game/desktop/DesktopLauncher.java
+++ desktop/src/com/bitdecay/game/desktop/DesktopLauncher.java
@@ -2,14 +2,13 @@ package com.bitdecay.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.bitdecay.game.Helm;
-import com.bitdecay.game.screen.GameScreen;
+import com.bitdecay.game.desktop.editor.HelmEditor;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1600;
 		config.height = 900;
-		new LwjglApplication(new Helm(), config);
+		new LwjglApplication(new HelmEditor(), config);
 	}
 }
