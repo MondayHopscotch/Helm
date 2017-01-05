a03c63b865140cc601e744fb5903cfdc7c38d53e
Logan Moore <monday.hopscotch@gmail.com>
2017-01-05 00:09:13 -0700

    options menu work related to steering settings

--- core/src/com/bitdecay/game/prefs/GamePrefs.java
+++ core/src/com/bitdecay/game/prefs/GamePrefs.java
@@ -8,6 +8,10 @@ public class GamePrefs {
 
     public static final String USE_DYNAMIC_STEERING_CONTROLS = "useDynamicSteeringControls";
     public static final boolean USE_DYNAMIC_STEERING_CONTROLS_DEFAULT = false;
+
     public static final String SIMPLE_STEERING_WIDTH = "simpleSteeringWidthRatio";
+    public static final float SIMPLE_STEERING_WIDTH_DEFAULT = 0.5f;
+
     public static final String SIMPLE_STEERING_HEIGHT = "simpleSteeringHeightRatio";
+    public static final float SIMPLE_STEERING_HEIGHT_DEFAULT = 0.5f;
 }
--- core/src/com/bitdecay/game/screen/OptionsScreen.java
+++ core/src/com/bitdecay/game/screen/OptionsScreen.java
@@ -2,13 +2,14 @@ package com.bitdecay.game.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Slider;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
@@ -26,6 +27,10 @@ public class OptionsScreen implements Screen {
     Stage stage;
     Skin skin;
     private final CheckBox dynamicSteeringInput;
+    private final Slider steeringXInput;
+    private final Slider steeringYInput;
+    private final Label steeringPositionYLabel;
+    private final Label steeringPositionXLabel;
 
     public OptionsScreen(final Helm game) {
         this.game = game;
@@ -42,12 +47,33 @@ public class OptionsScreen implements Screen {
         prefsTable.pad(200);
 
         Label dynamicSteeringLabel = new Label("Use Dynamic Steering", skin);
-        dynamicSteeringLabel.setFontScale(3);
+        dynamicSteeringLabel.setFontScale(5);
         dynamicSteeringInput = new CheckBox(null, skin);
         dynamicSteeringInput.setChecked(Helm.prefs.getBoolean(GamePrefs.USE_DYNAMIC_STEERING_CONTROLS, GamePrefs.USE_DYNAMIC_STEERING_CONTROLS_DEFAULT));
         dynamicSteeringInput.getImage().scaleBy(6);
         dynamicSteeringInput.align(Align.bottomLeft);
         dynamicSteeringInput.setOrigin(Align.bottomLeft);
+
+        dynamicSteeringInput.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                updateSteeringSettingVisibility();
+            }
+        });
+
+        steeringPositionXLabel = new Label("Steering Joystick X", skin);
+        steeringPositionXLabel.setFontScale(5);
+        steeringXInput = new Slider(.1f, .5f, .05f, false, skin);
+        steeringXInput.setAnimateDuration(0.1f);
+        // this affects all slider knobs
+        steeringXInput.getStyle().knob.setMinHeight(80);
+        steeringXInput.setValue(Helm.prefs.getFloat(GamePrefs.SIMPLE_STEERING_WIDTH, GamePrefs.SIMPLE_STEERING_WIDTH_DEFAULT));
+
+        steeringPositionYLabel = new Label("Steering Joystick Y", skin);
+        steeringPositionYLabel.setFontScale(5);
+        steeringYInput = new Slider(.1f, .5f, .05f, false, skin);
+        steeringYInput.setAnimateDuration(0.1f);
+        steeringYInput.setValue(Helm.prefs.getFloat(GamePrefs.SIMPLE_STEERING_HEIGHT, GamePrefs.SIMPLE_STEERING_HEIGHT_DEFAULT));
 
         prefsTable.add(dynamicSteeringLabel).align(Align.left).expandX();
         prefsTable.add(dynamicSteeringInput).size(
@@ -55,8 +81,16 @@ public class OptionsScreen implements Screen {
                 dynamicSteeringInput.getImage().getHeight() * dynamicSteeringInput.getImage().getScaleY()).align(Align.center);
         prefsTable.row();
 
+        prefsTable.add(steeringPositionXLabel).align(Align.left);
+        prefsTable.add(steeringXInput).width(300);
+        prefsTable.row();
 
-        TextButton doneLabel = new TextButton("Done", skin);
+        prefsTable.add(steeringPositionYLabel).align(Align.left);
+        prefsTable.add(steeringYInput).width(300);
+        prefsTable.row();
+
+
+        TextButton doneLabel = new TextButton("Save", skin);
         doneLabel.getLabel().setFontScale(7);
 
         doneLabel.addListener(new ClickListener() {
@@ -71,6 +105,16 @@ public class OptionsScreen implements Screen {
         mainTable.add(doneLabel);
 
         stage.addActor(mainTable);
+
+        updateSteeringSettingVisibility();
+    }
+
+    private void updateSteeringSettingVisibility() {
+        boolean usingDynamicSteering = dynamicSteeringInput.isChecked();
+        steeringPositionXLabel.setVisible(!usingDynamicSteering);
+        steeringXInput.setVisible(!usingDynamicSteering);
+        steeringPositionYLabel.setVisible(!usingDynamicSteering);
+        steeringYInput.setVisible(!usingDynamicSteering);
     }
 
     @Override
@@ -104,6 +148,8 @@ public class OptionsScreen implements Screen {
     @Override
     public void hide() {
         Helm.prefs.putBoolean(GamePrefs.USE_DYNAMIC_STEERING_CONTROLS, dynamicSteeringInput.isChecked());
+        Helm.prefs.putFloat(GamePrefs.SIMPLE_STEERING_WIDTH, steeringXInput.getValue());
+        Helm.prefs.putFloat(GamePrefs.SIMPLE_STEERING_HEIGHT, steeringYInput.getValue());
         Helm.prefs.flush();
     }
 
