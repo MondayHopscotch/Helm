8853faa02e042af1138c002a5cc483033263e573
Logan Moore <monday.hopscotch@gmail.com>
2017-02-16 23:21:57 -0700

    player explosion

--- core/src/com/bitdecay/game/system/CrashSystem.java
+++ core/src/com/bitdecay/game/system/CrashSystem.java
@@ -4,6 +4,8 @@ import com.bitdecay.game.GameEntity;
 import com.bitdecay.game.GamePilot;
 import com.bitdecay.game.component.CrashComponent;
 import com.bitdecay.game.component.SteeringControlComponent;
+import com.bitdecay.game.component.TransformComponent;
+import com.bitdecay.game.entities.ExplosionEntity;
 import com.bitdecay.game.sound.MusicLibrary;
 import com.bitdecay.game.sound.SFXLibrary;
 import com.bitdecay.game.sound.SoundMode;
@@ -21,11 +23,19 @@ public class CrashSystem extends AbstractIteratingGameSystem {
         entity.removeComponent(SteeringControlComponent.class);
         pilot.doMusic(SoundMode.STOP, MusicLibrary.SHIP_BOOST);
         pilot.doSound(SoundMode.PLAY, SFXLibrary.SHIP_CRASH);
-        pilot.requestRestartLevel();
+
+        TransformComponent transformComponent = entity.getComponent(TransformComponent.class);
+
+        ExplosionEntity explosionEntity = new ExplosionEntity(transformComponent.position);
+        levelPlayer.addEntity(explosionEntity);
+        levelPlayer.removeEntity(entity);
     }
 
     @Override
     public boolean canActOn(GameEntity entity) {
-        return entity.hasComponent(CrashComponent.class);
+        return entity.hasComponents(
+                CrashComponent.class,
+                TransformComponent.class
+        );
     }
 }
