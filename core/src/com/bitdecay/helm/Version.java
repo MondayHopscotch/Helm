ad67afcfac9ccd7003cbbfc7322c9713154f6aef
Logan Moore <monday.hopscotch@gmail.com>
2017-10-21 01:53:46 -0600

    roll version to 0.9.1

--- core/src/com/bitdecay/helm/Version.java
+++ core/src/com/bitdecay/helm/Version.java
@@ -4,5 +4,5 @@ package com.bitdecay.helm;
  * Created by Monday on 12/21/2016.
  */
 public class Version {
-    public static String CURRENT_VERSION = "0.9.0";
+    public static String CURRENT_VERSION = "0.9.1";
 }
