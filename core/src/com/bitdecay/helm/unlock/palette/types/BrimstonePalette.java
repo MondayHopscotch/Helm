f26030871d528e62011b481eedfdab9f1d2fae08
Logan Moore <monday.hopscotch@gmail.com>
2018-02-02 23:34:11 -0700

    Fix palettes and add the launch smoke colors

--- core/src/com/bitdecay/helm/unlock/palette/types/BrimstonePalette.java
+++ core/src/com/bitdecay/helm/unlock/palette/types/BrimstonePalette.java
@@ -12,6 +12,7 @@ public class BrimstonePalette extends GamePalette {
         colors.put(com.bitdecay.helm.unlock.palette.GameColors.LEVEL_SEGMENT, new Color(0xfad99aff));
         colors.put(com.bitdecay.helm.unlock.palette.GameColors.EXPLOSION, new Color(0xfad99aff));
         colors.put(com.bitdecay.helm.unlock.palette.GameColors.SHIP_BODY, new Color(0x965d63ff));
+        colors.put(com.bitdecay.helm.unlock.palette.GameColors.LAUNCH_SMOKE, new Color(0xfad99aff));
         colors.put(com.bitdecay.helm.unlock.palette.GameColors.FUEL_METER, new Color(0xf4b642ff));
         colors.put(com.bitdecay.helm.unlock.palette.GameColors.REPULSION_FIELD, new Color(0x842402ff));
         colors.put(com.bitdecay.helm.unlock.palette.GameColors.GRAVITY_WELL, new Color(0x842402ff));
--- core/src/com/bitdecay/helm/unlock/palette/types/MidnightPalette.java
+++ core/src/com/bitdecay/helm/unlock/palette/types/MidnightPalette.java
@@ -11,6 +11,7 @@ public class MidnightPalette extends com.bitdecay.helm.system.render.GamePalette
         colors.put(com.bitdecay.helm.unlock.palette.GameColors.LEVEL_SEGMENT, new Color(0x3D2645FF));
         colors.put(com.bitdecay.helm.unlock.palette.GameColors.EXPLOSION, new Color(0xD16666FF));
         colors.put(com.bitdecay.helm.unlock.palette.GameColors.SHIP_BODY, new Color(0x6E447CFF));
+        colors.put(com.bitdecay.helm.unlock.palette.GameColors.LAUNCH_SMOKE, new Color(0xD16666FF));
         colors.put(com.bitdecay.helm.unlock.palette.GameColors.LAUNCH_SMOKE, Color.WHITE);
         colors.put(com.bitdecay.helm.unlock.palette.GameColors.FUEL_METER, new Color(0x1DA58CFF));
         colors.put(com.bitdecay.helm.unlock.palette.GameColors.REPULSION_FIELD, new Color(0x136A5AFF));
